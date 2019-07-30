f6f76f66020418b3af40d1640038dd98fdd94994
Iqbal Ahmed <iqbal@iqbal.com>
2019-07-30 22:56:36 +0100

    TopStoriesPanel - Add React Story

--- src/stories/index.js
+++ src/stories/index.js
@@ -5,6 +5,11 @@ import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import CountrySelectorPanel from '../components/CountrySelectorPanel'
+import TopStoriesPanel from '../components/TopStoriesPanel'
 
 storiesOf('CountrySelectorPanel', module)
   .add('default state', () => <CountrySelectorPanel />)
+
+storiesOf('TopStoriesPanel', module)
+  .add('default state', () => <TopStoriesPanel />)
+
