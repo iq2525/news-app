c8535fc6e91053cbd89fc3519f5f4ffce9a89be6
Iqbal Ahmed <iqbal@iqbal.com>
2019-07-30 20:44:16 +0100

    Add CountrySelectorPanel with Unit Test

---
+++ src/components/CountrySelectorPanel.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {shallow} from 'enzyme'
+
+import CountrySelectorPanel from './CountrySelectorPanel'
+
+describe('<CountrySelectorPanel />', () => {
+    it('renders component', () => {
+      const wrapper = shallow(<CountrySelectorPanel/>)
+    })
+})
