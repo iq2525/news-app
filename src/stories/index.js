1eece3a526835b288c309c8c76092e4b5913837a
Iqbal Ahmed <iqbal@iqbal.com>
2019-07-31 23:05:58 +0100

    CountrySelectorPanel - Add to Storybook.

--- src/stories/index.js
+++ src/stories/index.js
@@ -7,8 +7,33 @@ import { linkTo } from '@storybook/addon-links';
 import CountrySelectorPanel from '../components/CountrySelectorPanel'
 import TopStoriesPanel from '../components/TopStoriesPanel'
 
+const propsCountrySelectorPanel = {
+  countries: [
+    {
+      name: 'Australia',
+      code: 'au'
+    },
+    {
+      name: 'France',
+      code: 'fr'
+    },
+    {
+      name: 'India',
+      code: 'in'
+    },
+    {
+      name: 'UK',
+      code: 'au'
+    },
+    {
+      name: 'USA',
+      code: 'au'
+    }
+  ]
+}
+
 storiesOf('CountrySelectorPanel', module)
-  .add('default state', () => <CountrySelectorPanel />)
+  .add('default state', () => <CountrySelectorPanel {...propsCountrySelectorPanel}/>)
 
 storiesOf('TopStoriesPanel', module)
   .add('default state', () => <TopStoriesPanel />)
