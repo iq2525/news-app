256ae6563e2436bf7bdf05aa5ff686621218100f
Iqbal Ahmed <iqbal@iqbal.com>
2019-07-31 23:00:57 +0100

    CountrySelectorPanel - Change to use Array properties, add PropTypes and Unit Test.

--- src/components/CountrySelectorPanel.js
+++ src/components/CountrySelectorPanel.js
@@ -1,21 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 const CountryButton = styled.button`
   width: 300px;
 `
 CountryButton.displayName = 'CountryButton'
 
-const CountrySelectorPanel = () => {
+const CountrySelectorPanel = ({ countries }) => {
   return (
   <div>
-      <CountryButton>Australia</CountryButton>
-      <CountryButton>France</CountryButton>
-      <CountryButton>India</CountryButton>
-      <CountryButton>UK</CountryButton>
-      <CountryButton>USA</CountryButton>
+    {countries.map((country, id) => <CountryButton key={id}>{country.name}</CountryButton>)}
   </div>
   )
+}
+
+CountrySelectorPanel.propTypes = {
+  countries: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      code: PropTypes.string
+    })
+  ).isRequired,
 }
 
 export default CountrySelectorPanel
--- src/components/CountrySelectorPanel.test.js
+++ src/components/CountrySelectorPanel.test.js
@@ -3,9 +3,34 @@ import { shallow } from 'enzyme'
 
 import CountrySelectorPanel from './CountrySelectorPanel'
 
+const props = {
+  countries: [
+    {
+      name: 'Australia',
+      code: 'au'
+    },
+    {
+      name: 'France',
+      code: 'fr'
+    },
+    {
+      name: 'India',
+      code: 'in'
+    },
+    {
+      name: 'UK',
+      code: 'au'
+    },
+    {
+      name: 'USA',
+      code: 'au'
+    }
+  ]
+}
+
 describe('<CountrySelectorPanel />', () => {
     it('renders component', () => {
-      const wrapper = shallow(<CountrySelectorPanel/>)
+      const wrapper = shallow(<CountrySelectorPanel {...props}/>)
 
       expect(wrapper.find('CountryButton').length).toEqual(5)
     })
