8c4808ee8fd3f3dabf6e57615702ce4b7de204d1
Iqbal Ahmed <iqbal@iqbal.com>
2019-07-31 23:19:42 +0100

    TopStoriesPanel - Formatting updates

--- src/components/TopStoriesPanel.js
+++ src/components/TopStoriesPanel.js
@@ -1,25 +1,23 @@
-import React from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
 const Story = styled.div`
   width: 100%;
   padding: 10px;
-`
-Story.displayName = 'Story'
+`;
+Story.displayName = "Story";
 
 const TopStoriesPanel = ({ stories }) => {
   return (
-  <div>
-      {stories && 
-        stories.map((story, id) => <Story key={id}>{story}</Story>)
-      }
-  </div>
-  )
-}
+    <div>
+      {stories && stories.map((story, id) => <Story key={id}>{story}</Story>)}
+    </div>
+  );
+};
 
 TopStoriesPanel.propTypes = {
   stories: PropTypes.arrayOf(PropTypes.string)
-}
+};
 
-export default TopStoriesPanel
+export default TopStoriesPanel;
--- src/components/TopStoriesPanel.test.js
+++ src/components/TopStoriesPanel.test.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import { shallow } from 'enzyme'
+import React from "react";
+import { shallow } from "enzyme";
 
-import TopStoriesPanel from './TopStoriesPanel'
+import TopStoriesPanel from "./TopStoriesPanel";
 
 const props = {
-  stories: ['Story 1', 'Story 2',]
-}
+  stories: ["Story 1", "Story 2"]
+};
 
-describe('<TopStoriesPanel />', () => {
-  it('renders component with no stories', () => {
-    const wrapper = shallow(<TopStoriesPanel />)
+describe("<TopStoriesPanel />", () => {
+  it("renders component with no stories", () => {
+    const wrapper = shallow(<TopStoriesPanel />);
 
-    expect(wrapper.find('Story').length).toEqual(0)
-  })
+    expect(wrapper.find("Story").length).toEqual(0);
+  });
 
-  it('renders component with 2 stories', () => {
-    const wrapper = shallow(<TopStoriesPanel {...props}/>)
+  it("renders component with 2 stories", () => {
+    const wrapper = shallow(<TopStoriesPanel {...props} />);
 
-    expect(wrapper.find('Story').length).toEqual(2)
-  })
-})
+    expect(wrapper.find("Story").length).toEqual(2);
+  });
+});
