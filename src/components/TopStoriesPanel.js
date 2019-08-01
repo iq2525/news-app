a9f298787556c2267dbf73606e821925c6249c0d
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-01 22:11:40 +0100

    TopStoriesPanel - Add loading state and Test

--- src/components/TopStoriesPanel.js
+++ src/components/TopStoriesPanel.js
@@ -2,22 +2,32 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
+const Container = styled.div`
+  width: 100%;
+  padding: 10px;
+`;
+Container.displayName = "Container";
+
 const Story = styled.div`
   width: 100%;
   padding: 10px;
 `;
 Story.displayName = "Story";
 
-const TopStoriesPanel = ({ stories }) => {
+const TopStoriesPanel = ({ stories, isLoading }) => {
   return (
-    <div>
-      {stories && stories.map((story, id) => <Story key={id}>{story}</Story>)}
-    </div>
+    <Container>
+      {isLoading
+        ? "Loading..."
+        : stories &&
+          stories.map((story, id) => <Story key={id}>{story}</Story>)}
+    </Container>
   );
 };
 
 TopStoriesPanel.propTypes = {
-  stories: PropTypes.arrayOf(PropTypes.string)
+  stories: PropTypes.arrayOf(PropTypes.string),
+  isLoading: PropTypes.bool.isRequired
 };
 
 export default TopStoriesPanel;
--- src/components/TopStoriesPanel.test.js
+++ src/components/TopStoriesPanel.test.js
@@ -4,22 +4,34 @@ import { shallow } from "enzyme";
 import TopStoriesPanel from "./TopStoriesPanel";
 
 const props = {
-  stories: ["Story 1", "Story 2"]
+  stories: ["Story 1", "Story 2", "Story 3", "Story 4", "Story 5"],
+  isLoading: false
 };
 
 describe("<TopStoriesPanel />", () => {
   it("renders component with no stories", () => {
-    const wrapper = shallow(<TopStoriesPanel />);
+    const wrapper = shallow(<TopStoriesPanel isLoading={false} />);
 
     expect(wrapper.find("Story").length).toEqual(0);
   });
 
-  it("renders component with 2 stories", () => {
+  it("renders component with 5 stories", () => {
     const wrapper = shallow(<TopStoriesPanel {...props} />);
 
     const stories = wrapper.find("Story");
-    expect(stories.length).toEqual(2);
+    expect(stories.length).toEqual(5);
     expect(stories.at(0).text()).toEqual(props.stories[0]);
     expect(stories.at(1).text()).toEqual(props.stories[1]);
+    expect(stories.at(2).text()).toEqual(props.stories[2]);
+    expect(stories.at(3).text()).toEqual(props.stories[3]);
+    expect(stories.at(4).text()).toEqual(props.stories[4]);
+  });
+
+  it("renders component with loading state", () => {
+    const propsLoading = { ...props, isLoading: true };
+    const wrapper = shallow(<TopStoriesPanel {...propsLoading} />);
+
+    const stories = wrapper.find("Story");
+    expect(stories.length).toEqual(0);
   });
 });
