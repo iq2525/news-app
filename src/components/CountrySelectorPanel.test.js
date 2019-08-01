fc94bd1a37da12fea9b9fb1f564f0b7e888f1139
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-01 20:55:03 +0100

    CountrySelectorPanel - Update test to check for Country name.

--- src/components/CountrySelectorPanel.test.js
+++ src/components/CountrySelectorPanel.test.js
@@ -1,37 +1,43 @@
-import React from 'react';
-import { shallow } from 'enzyme'
+import React from "react";
+import { shallow } from "enzyme";
 
-import CountrySelectorPanel from './CountrySelectorPanel'
+import CountrySelectorPanel from "./CountrySelectorPanel";
 
 const props = {
   countries: [
     {
-      name: 'Australia',
-      code: 'au'
+      name: "Australia",
+      code: "au"
     },
     {
-      name: 'France',
-      code: 'fr'
+      name: "France",
+      code: "fr"
     },
     {
-      name: 'India',
-      code: 'in'
+      name: "India",
+      code: "in"
     },
     {
-      name: 'UK',
-      code: 'au'
+      name: "UK",
+      code: "au"
     },
     {
-      name: 'USA',
-      code: 'au'
+      name: "USA",
+      code: "au"
     }
   ]
-}
+};
 
-describe('<CountrySelectorPanel />', () => {
-    it('renders component', () => {
-      const wrapper = shallow(<CountrySelectorPanel {...props}/>)
+describe("<CountrySelectorPanel />", () => {
+  it("renders component", () => {
+    const wrapper = shallow(<CountrySelectorPanel {...props} />);
 
-      expect(wrapper.find('CountryButton').length).toEqual(5)
-    })
-})
+    const buttons = wrapper.find("CountryButton");
+    expect(buttons.length).toEqual(5);
+    expect(buttons.at(0).text()).toEqual(props.countries[0].name);
+    expect(buttons.at(1).text()).toEqual(props.countries[1].name);
+    expect(buttons.at(2).text()).toEqual(props.countries[2].name);
+    expect(buttons.at(3).text()).toEqual(props.countries[3].name);
+    expect(buttons.at(4).text()).toEqual(props.countries[4].name);
+  });
+});
