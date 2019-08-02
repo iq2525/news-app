31f83b8158f5f643b64c6c2ea04d58ab8a5964e5
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-02 22:01:46 +0100

    Create Constants file and add Countries

---
+++ src/constants/app.js
@@ -0,0 +1,22 @@
+export const countries = [
+  {
+    name: "Australia",
+    code: "au"
+  },
+  {
+    name: "France",
+    code: "fr"
+  },
+  {
+    name: "India",
+    code: "in"
+  },
+  {
+    name: "UK",
+    code: "au"
+  },
+  {
+    name: "USA",
+    code: "au"
+  }
+];
