5a461c66c55dbd62c50cddf22b858b9e5f8e5e53
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-02 19:42:50 +0100

    Moves Countries array to constants file.

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,35 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import TopStoriesPanel from "../components/TopStoriesPanel";
 import CountrySelectorPanel from "../components/CountrySelectorPanel";
+import { countries } from "../constants/app";
 
 const Content = styled.section`
   width: 100%;
   padding: 10px;
 `;
 Content.displayName = "Content";
-
-const countries = [
-  {
-    name: "Australia",
-    code: "au"
-  },
-  {
-    name: "France",
-    code: "fr"
-  },
-  {
-    name: "India",
-    code: "in"
-  },
-  {
-    name: "UK",
-    code: "au"
-  },
-  {
-    name: "USA",
-    code: "au"
-  }
-];
 
 const App = ({ stories, isLoading, isError }) => {
   return (
