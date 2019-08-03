497417351322898f6489f6971c146958d7205937
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-03 20:22:11 +0100

    News API Enpoint - Add to constants file

--- src/constants/app.js
+++ src/constants/app.js
@@ -24,3 +24,9 @@ export const COUNTRIES = [
 export const REQUEST_STORIES = "REQUEST_STORIES";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_FAILURE = "FETCH_FAILURE";
+
+const NEWS_API_ENDPOINT = "https://newsapi.org/v2/top-headlines";
+const NEWS_API_KEY = "apiKey=971ea0c019c44f2295c42cb972032a67";
+const NEWS_API_COUNTRY_PARAM = "country=us";
+const NEWS_API_PAGE_SIZE = "pageSize=5";
+export const NEWS_API_ENDPOINT_WITH_PARAMS = `${NEWS_API_ENDPOINT}?${NEWS_API_COUNTRY_PARAM}&${NEWS_API_PAGE_SIZE}&${NEWS_API_KEY}`;
