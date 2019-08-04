e6177a832b295752e35edcd046362cdaa15f65f2
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-04 10:45:02 +0100

    New Styling Constants file - Add cours to file

---
+++ src/constants/styling.js
@@ -0,0 +1,5 @@
+export const COLOUR_GREY = "#767676";
+export const COLOUR_LIGHT_GREY = "#DCDCDC";
+export const COLOUR_BLACK = "#000";
+export const COLOUR_LIGHT_BLUE = "#1167A8";
+export const COLOUR_PURPLE = "#8B008B";
