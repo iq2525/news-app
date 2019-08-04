0ae1bd42c7d4c9fa0ff3a25989712e4345a78414
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-04 11:59:48 +0100

    CountrySelectorPanel - Add instructions for selecting a country.

--- src/components/CountrySelectorPanel.js
+++ src/components/CountrySelectorPanel.js
@@ -2,35 +2,52 @@ import React from "react";
 import PropTypes from "prop-types";
 import CountryButton from "./CountryButton";
 import styled from "styled-components";
-import { COLOUR_LIGHT_GREY } from "../constants/styling";
+import { COLOUR_LIGHT_GREY, BREAKPOINTS } from "../constants/styling";
 
 const Section = styled.section`
+  border: 1px solid ${COLOUR_LIGHT_GREY};
+  border-radius: 5px;
+`;
+
+const Description = styled.div`
+  font-family: Arial, Helvetica, sans-serif;
+  margin: 10px 5px;
+`;
+
+const AllButtonsContainer = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-wrap: wrap;
-  border: 1px solid ${COLOUR_LIGHT_GREY};
-  border-radius: 5px;
 `;
 
 const ButtonContainer = styled.div`
   box-sizing: border-box;
   min-width: 50%;
-  padding: 2px;
+  padding: 5px;
+
+  @media (min-width: ${BREAKPOINTS.medium}) {
+    min-width: 20%;
+  }
 `;
 
 const CountrySelectorPanel = ({ countries, isLoading, fetchStories }) => {
   return (
     <Section>
-      {countries.map((country, id) => (
-        <ButtonContainer>
-          <CountryButton
-            key={id}
-            country={country}
-            isLoading={isLoading}
-            fetchStories={fetchStories}
-          />
-        </ButtonContainer>
-      ))}
+      <Description>
+        Click a Country to see the Top 5 Stories from there below:
+      </Description>
+      <AllButtonsContainer>
+        {countries.map((country, id) => (
+          <ButtonContainer>
+            <CountryButton
+              key={id}
+              country={country}
+              isLoading={isLoading}
+              fetchStories={fetchStories}
+            />
+          </ButtonContainer>
+        ))}
+      </AllButtonsContainer>
     </Section>
   );
 };
