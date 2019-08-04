0c0f0a0b221625ef1f905af262ff1b01d261f8e4
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-04 11:03:40 +0100

    CountryButton - Add styling with Hover states.

--- src/components/CountryButton.js
+++ src/components/CountryButton.js
@@ -1,9 +1,35 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import {
+  COLOUR_LIGHT_GREY,
+  COLOUR_BLUE,
+  COLOUR_BLACK,
+  COLOUR_WHITE,
+  COLOUR_LIGHT_BLUE
+} from "../constants/styling";
 
 const Button = styled.button`
-  width: 300px;
+  box-sizing: border-box;
+  width: 50%;
+  height: 50px;
+  margin: 10px;
+  border-radius: 5px;
+  background-color: ${COLOUR_LIGHT_GREY};
+  color: ${COLOUR_BLACK};
+  font-weight: bold;
+  font-size: 13px;
+  cursor: pointer;
+
+  &:hover {
+    color: ${COLOUR_WHITE};
+    background-color: ${COLOUR_BLUE};
+  }
+
+  &:active {
+    color: ${COLOUR_WHITE};
+    background-color: ${COLOUR_LIGHT_BLUE};
+  }
 `;
 Button.displayName = "Button";
 
--- src/constants/styling.js
+++ src/constants/styling.js
@@ -1,5 +1,7 @@
 export const COLOUR_GREY = "#767676";
 export const COLOUR_LIGHT_GREY = "#DCDCDC";
 export const COLOUR_BLACK = "#000";
+export const COLOUR_WHITE = "#fff";
+export const COLOUR_BLUE = "#10619f";
 export const COLOUR_LIGHT_BLUE = "#1167A8";
 export const COLOUR_PURPLE = "#8B008B";
