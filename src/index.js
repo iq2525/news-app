6e2eb7f982a32c02cc649a69a8d8079e529d8579
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-04 21:58:40 +0100

    Update to use Redux Dev Tools and remove Redux Logger.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { render } from "react-dom";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
-import { createLogger } from "redux-logger";
 import reducer from "./reducers/reducer";
 import App from "./containers/App";
 
 const middleware = [thunk];
-if (process.env.NODE_ENV !== "production") {
-  middleware.push(createLogger());
-}
 
-const store = createStore(reducer, applyMiddleware(...middleware));
+const reduxDevTools =
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(...middleware),
+    reduxDevTools
+  )
+);
 
 render(
   <Provider store={store}>
