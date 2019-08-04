1c67d9f9504533cca40bee75981ee7cb9f450610
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-04 11:53:47 +0100

    Styling - Add breakpoint constants.

--- src/constants/styling.js
+++ src/constants/styling.js
@@ -6,3 +6,10 @@ export const COLOUR_BLUE = "#10619f";
 export const COLOUR_LIGHT_BLUE = "#1167A8";
 export const COLOUR_LIGHT_BLUE_2 = "#CFE0E8";
 export const COLOUR_PURPLE = "#8B008B";
+
+export const BREAKPOINTS = {
+  small: "320px",
+  medium: "768px",
+  large: "992px",
+  xlarge: "1200px"
+};
