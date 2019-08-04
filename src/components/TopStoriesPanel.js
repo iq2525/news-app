074cd4cc28a3c4a399c13b2068ce1197e2aed971
Iqbal Ahmed <iqbal@iqbal.com>
2019-08-04 07:27:14 +0100

    TopStoriesPanel - Refactor to use new Story component.

--- src/components/TopStoriesPanel.js
+++ src/components/TopStoriesPanel.js
@@ -1,18 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import Story from "./Story";
 
 const Container = styled.div`
   width: 100%;
   padding: 10px;
 `;
 Container.displayName = "Container";
-
-const Story = styled.section`
-  width: 100%;
-  padding: 10px;
-`;
-Story.displayName = "Story";
 
 const TopStoriesPanel = ({ stories, isLoading, isError }) => {
   let content;
@@ -22,14 +17,21 @@ const TopStoriesPanel = ({ stories, isLoading, isError }) => {
   } else if (isLoading) {
     content = "Loading...";
   } else if (stories && stories.length) {
-    content = stories.map((story, id) => <Story key={id}>{story}</Story>);
+    content = stories.map((story, id) => <Story key={id} story={story} />);
   }
 
   return <Container>{content}</Container>;
 };
 
 TopStoriesPanel.propTypes = {
-  stories: PropTypes.arrayOf(PropTypes.string),
+  stories: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string,
+      description: PropTypes.string,
+      url: PropTypes.string,
+      urlToImage: PropTypes.string
+    })
+  ),
   isLoading: PropTypes.bool.isRequired,
   isError: PropTypes.bool.isRequired
 };
--- src/components/TopStoriesPanel.test.js
+++ src/components/TopStoriesPanel.test.js
@@ -4,7 +4,38 @@ import { shallow } from "enzyme";
 import TopStoriesPanel from "./TopStoriesPanel";
 
 const props = {
-  stories: ["Story 1", "Story 2", "Story 3", "Story 4", "Story 5"],
+  stories: [
+    {
+      title: "Story 1",
+      description: "Description 1",
+      url: "http://test.com/1",
+      urlToImage: "http://test.com/image1.jpg"
+    },
+    {
+      title: "Story 2",
+      description: "Description 2",
+      url: "http://test.com/2",
+      urlToImage: "http://test.com/image.jpg/2"
+    },
+    {
+      title: "Story 3",
+      description: "Description 3",
+      url: "http://test.com/3",
+      urlToImage: "http://test.com/image3.jpg"
+    },
+    {
+      title: "Story 4",
+      description: "Description 4",
+      url: "http://test.com/4",
+      urlToImage: "http://test.com/image4.jpg"
+    },
+    {
+      title: "Story 5",
+      description: "Description 5",
+      url: "http://test.com/5",
+      urlToImage: "http://test.com/image5.jpg"
+    }
+  ],
   isLoading: false,
   isError: false
 };
@@ -12,10 +43,11 @@ const props = {
 describe("<TopStoriesPanel />", () => {
   it("renders component with no stories", () => {
     const wrapper = shallow(
-      <TopStoriesPanel isLoading={false} isError={false} />
+      <TopStoriesPanel isLoading={false} isError={false} stories={null} />
     );
 
-    expect(wrapper.find("Story").length).toEqual(0);
+    const stories = wrapper.find("Story");
+    expect(stories.length).toEqual(0);
   });
 
   it("renders component with 5 stories", () => {
@@ -23,11 +55,12 @@ describe("<TopStoriesPanel />", () => {
 
     const stories = wrapper.find("Story");
     expect(stories.length).toEqual(5);
-    expect(stories.at(0).text()).toEqual(props.stories[0]);
-    expect(stories.at(1).text()).toEqual(props.stories[1]);
-    expect(stories.at(2).text()).toEqual(props.stories[2]);
-    expect(stories.at(3).text()).toEqual(props.stories[3]);
-    expect(stories.at(4).text()).toEqual(props.stories[4]);
+    console.log("stories.debug(): ", stories.debug());
+    expect(stories.at(0).prop("story")).toEqual(props.stories[0]);
+    expect(stories.at(1).prop("story")).toEqual(props.stories[1]);
+    expect(stories.at(2).prop("story")).toEqual(props.stories[2]);
+    expect(stories.at(3).prop("story")).toEqual(props.stories[3]);
+    expect(stories.at(4).prop("story")).toEqual(props.stories[4]);
   });
 
   it("renders component with loading state", () => {
